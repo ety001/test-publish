c4c17cd90d83711905b3079bc6e608386edcc4bd
ety001 <work@akawa.ink>
2024-12-13 15:56:21 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,61 +1,75 @@
 import path from 'path';
-import { CleanWebpackPlugin } from 'clean-webpack-plugin';
-
 const __dirname = path.dirname(new URL(import.meta.url).pathname);
 
-const config = {
-  entry: './src/index.ts',
-  mode: 'production',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'steem.min.js',
-    library: {
-      name: 'steem',
-      type: 'umd',
+export default [
+  {
+    // build for esm
+    entry: './src/index.ts',
+    output: {
+      filename: 'test.esm.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'module'
     },
-    globalObject: 'this',
-  },
-  devServer: {
-    open: true,
-    host: 'localhost',
-  },
-  plugins: [
-    // new CleanWebpackPlugin(),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(ts|tsx)$/i,
-        loader: 'ts-loader',
-        exclude: ['/node_modules/'],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.jsx', '.js'],
-  },
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        default: false,
-        vendors: false,
-      },
+    experiments: {
+      outputModule: true
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/i,
+          loader: 'ts-loader',
+          exclude: ['/node_modules/'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
     },
   },
-  experiments: {
-    outputModule: true,
-  },
-};
-
-const esmConfig = {
-  ...config,
-  output: {
-    ...config.output,
-    filename: 'steem.esm.js',
-    library: {
-      type: 'module',
+  {
+    // build for cjs
+    entry: './src/index.ts',
+    output: {
+      filename: 'test.cjs.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'commonjs2'
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/i,
+          loader: 'ts-loader',
+          exclude: ['/node_modules/'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
     },
   },
-};
-
-export default [config, esmConfig];
+  {
+    // build for browser
+    entry: './src/index.ts',
+    output: {
+      filename: 'test.browser.js',
+      path: path.resolve(__dirname, 'dist'),
+      library: 'Steem',
+      libraryTarget: 'umd'
+    },
+    mode: 'production',
+    module: {
+      rules: [
+        {
+          test: /\.(ts|tsx)$/i,
+          loader: 'ts-loader',
+          exclude: ['/node_modules/'],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.ts', '.js'],
+    },
+  }
+];
