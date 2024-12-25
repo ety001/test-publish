8e3f5de947e5f199cb9d6b9756514fccdf306584
ety001 <work@akawa.ink>
2024-12-25 20:29:40 +0800

    update

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -6,7 +6,29 @@ import { defineUserConfig } from 'vuepress'
 
 export default defineUserConfig({
   bundler: viteBundler(),
-  theme: defaultTheme(),
+  theme: defaultTheme({
+    locales: {
+      '/': {
+        selectLanguageName: 'English',
+      },
+      '/zh/': {
+        selectLanguageName: '简体中文',
+      },
+    },
+  }),
   title: packageJson.name,
   description: packageJson.description,
+  lang: 'en-US',
+  locales: {
+    '/': {
+      lang: 'en-US',
+      title: packageJson.name,
+      description: packageJson.description,
+    },
+    '/zh/': {
+      lang: 'zh-CN',
+      title: packageJson.name,
+      description: packageJson.description,
+    },
+  },
 })
